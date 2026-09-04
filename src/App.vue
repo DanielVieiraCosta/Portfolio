e3401a8ab586df9dc868a2735ea25682caf3add5
DanielVieiraCosta <dvieiradacosta777@gmail.com>
2026-09-04 17:48:31 -0300

    correção do posicionamento da navbar

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,6 @@ import Home from './views/Home.vue';
 </script>
 
 <template>
-  <header class="navbar_custom">
-    <nav class="navbar fixed-top navbar-expand-lg fs-5">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Daniel Vieira da Costa</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav d-flex ms-auto gap-2">
-            <a class="nav-link" href="#projetos">Projetos</a>
-            <a class="nav-link" href="#">Sobre</a>
-            <a class="nav-link" href="#">Contato</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
   <main class="plano_de_fundo min-vh-100">
     <Home />
   </main>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,21 @@ import TheWelcome from '@/components/TheWelcome.vue';
 </script>
 
 <template>
+  <nav class="navbar_custom navbar fixed-top navbar-expand-lg border-bottom border-2 fs-5">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Daniel Vieira da Costa</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav d-flex ms-auto gap-2">
+          <a class="nav-link" href="#projetos">Projetos</a>
+          <a class="nav-link" href="#">Sobre</a>
+          <a class="nav-link" href="#">Contato</a>
+        </div>
+      </div>
+    </div>
+  </nav>
   <main class="container">
     <TheWelcome />
     <Projects />
